623fe80756bc862052d0a9f186ea7f90db82ba6d
joylijing <joylijing@outlook.com>
2025-03-08 10:16:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def upload():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
